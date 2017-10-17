e5f833de11c889873a96956c005625a39193e0c7
AST <21300423@handong.edu>
2017-10-17 10:36:38 +0900

    finish 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,16 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  fams: Array<any> = [];
   constructor(public navCtrl: NavController) {
-
+    this.fams = [
+      {name:'Father', phone_number: '010-1234-0000', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 벧엘관', status: 'default'},
+      {name:'Mother', phone_number: '010-1234-0001', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 은혜관', status: 'default'},
+      {name:'Sister', phone_number: '010-1234-0002', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 창조관', status: 'default'},
+      {name:'Brother', phone_number: '010-1234-0003', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 비전관', status: 'default'}
+      ];
   }
-
+  addItem(){
+    
+  }
 }
