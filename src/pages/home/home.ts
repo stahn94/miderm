480dffa96aee74d4c40444be2e4001a9eb8ff7e0
AST <21300423@handong.edu>
2017-10-17 10:51:31 +0900

    finish -4

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,6 +16,9 @@ export class HomePage {
       ];
   }
   addItem(){
-    
+    this.fams.push({name:'Grandfather', phone_number: '010-1234-0004', address: '경북 포항시 북구 흥해읍 한동로 558 한동대학교 행복관', status: 'default'})
+  }
+  Ilikeit(fam: any){
+    fam.status = "like";
   }
 }
